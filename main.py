a88f5e6991ad48109468437d6bc25d86f5c42d1c
Finn Holme <finninde@stud.ntnu.no>
2019-04-25 12:10:27 +0200

    Add seed and buffer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
-import random
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+SEED = 100
 
 class Neuron():
     def __init__(self, max_connections, tiring_rate):
@@ -12,7 +12,7 @@ class Neuron():
         self.firing_probability = 0
         self.tiredness = 0
         # Juster hvor mye av nettet jeg starter med å aktivere
-        self.active_prev = np.random.randint(0,2)
+        self.active_old = 0
         self.active = np.random.randint(0,2)
         self.first_run = True
         self.max_connections = max_connections
@@ -43,10 +43,10 @@ class Neuron():
         else:
             self.firing_probability = np.clip(0.5 + (self.inp/2*(self.max_connections+1)) - self.tiredness, 0, 1)
         self.inp = 0
-        if random.random() < self.firing_probability:
-            self.active = 1
+        if np.random.random() < self.firing_probability:
+            self.active_old = 1
         else:
-            self.active = 0        
+            self.active_old = 0        
 
 def update_state(neurons):
     for neuron in neurons:
@@ -58,10 +58,14 @@ def active_neurons(neurons):
         if neuron.active == 1:
             active = active + 1
     return active
-            
-def simulation(max_connections, tiring_rate, number_of_neurons, simulation_steps, results, res_ind):
-    neurons = []
 
+def update_active_old(neurons):
+    for neuron in neurons:
+        neuron.active = neuron.active_old    
+
+def simulation(max_connections, tiring_rate, number_of_neurons, simulation_steps, results, res_ind, seed):
+    neurons = []
+    np.random.seed(seed)
     for i in range(0,number_of_neurons):
         neurons.append(Neuron(max_connections, tiring_rate))
 
@@ -73,9 +77,9 @@ def simulation(max_connections, tiring_rate, number_of_neurons, simulation_steps
     for i in range(0, simulation_steps):
         update_state(neurons)
         activity.append(active_neurons(neurons))
+        update_active_old(neurons)
 
     results.loc[res_ind] = [max_connections, tiring_rate, number_of_neurons, simulation_steps, np.asarray(activity)]
-    
 
 if __name__=="__main__":
     simulation_steps = 20
@@ -87,9 +91,9 @@ if __name__=="__main__":
     '''for i in range(0,100):
         simulation(4,0.1,10,100,results,  i)'''
     for tiring_rate in tiring_rates:
-        for number_of_neurons in tqdm(range(3,100)):
+        for n_neurons in tqdm(range(3,number_of_neurons)):
             for max_connections in range(1,50):
-                simulation(max_connections, tiring_rate, number_of_neurons, simulation_steps, results, res_ind)
+                simulation(max_connections, tiring_rate, n_neurons, simulation_steps, results, res_ind, SEED)
                 res_ind = res_ind+1
     results.to_pickle('res.pckl')
     
