10c24a5fc08b6be0e22c15640e4f38c163c83e12
Finn Holme <finninde@stud.ntnu.no>
2019-07-21 19:00:00 +0200

    add type for perceptron

--- readout_layer.py
+++ readout_layer.py
@@ -4,14 +4,14 @@ from sklearn.linear_model import LinearRegression, Perceptron
 class ReadoutLayer():
     def __init__(self, layer_type, dim):
         if layer_type == "linear":
-            self.type = "linear"
+            self.type = layer_type
             self.reg = LinearRegression()
 
         elif layer_type == "perceptron":
             self.reg = Perceptron()
             self.type = layer_type
 
-        else :
+        else:
             self.type = "linear"
             self.reg = LinearRegression()
         self.dim = dim
