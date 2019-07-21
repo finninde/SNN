078ae41423c6087bc9bbe740ac9f580715b776e8
Finn Holme <finninde@stud.ntnu.no>
2019-07-21 21:54:17 +0200

    support for perceptron

--- readout_layer.py
+++ readout_layer.py
@@ -17,7 +17,13 @@ class ReadoutLayer():
         self.dim = dim
 
     def predict(self, X):
-        return self.reg.predict(X)
+        if self.type == "linear":
+            return self.reg.predict(X)
+        elif self.type == "perceptron":
+            return self.reg.predict(X)
 
     def train(self, X, y):
-        self.reg.fit(X,y)
+        if self.type == "linear":
+            self.reg.fit(X,y)
+        elif self.type == "perceptron":
+            return self.reg.predict(X)
